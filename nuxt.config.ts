8cedbb65c579b0d194b6a89cae94c1e7227d9c19
Robert Scott-Buccleuch <github@coveted.name>
2025-12-07 14:54:58 -0800

    ported pages over but we are currently broken

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,29 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  css: ['~/assets/css/main.css'],
+ 
+  routeRules: {
+    '/api/**': {
+      cors: true
+    }
+  },
+
   nitro: {
     preset: "cloudflare_module",
 
     cloudflare: {
       deployConfig: true,
       nodeCompat: true
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
     }
   },
 
